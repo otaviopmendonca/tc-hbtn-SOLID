9779f30c6ea91303bef83455c7244fe2881b915a
otaviopmendonca <otaviopedroso.mendonca@gmail.com>
2025-08-13 14:27:07 -0300

    refactor: corrige nomenclatura do add.Handler

--- 0x04/log/LoggerInFile.java
+++ 0x04/log/LoggerInFile.java
@@ -8,7 +8,7 @@ public class LoggerInFile {
     public static void main(String[] args) {
         Logger logger = Logger.getLogger("MyLog");
         FileHandler fileHandler = new FileHandler("logs.txt");
-        logger.AddHandler(fileHandler);
+        logger.addHandler(fileHandler);
         SimpleFormatter simpleFormatter = new SimpleFormatter();
         fileHandler.setFormatter(simpleFormatter);
         logger.info("teste de Log");
