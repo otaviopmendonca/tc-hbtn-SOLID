72fa08f3b9e20232d4af5dc6f2f9655bfe802bdd
otaviopmendonca <otaviopedroso.mendonca@gmail.com>
2025-08-13 14:04:25 -0300

    feat: Aplica clean code em convencoes de nomenclatura

---
+++ 0x04/convencao/UserModel.java
@@ -0,0 +1,23 @@
+// classe users, representando nossa entidade
+
+class UsersModels {
+
+    int idUser;
+
+    String descricao;
+
+    final static int idadeMinima = 18;
+
+// função que coleta todos os users
+
+    public fetchUsers() { return [];}
+
+// função que coleta todos os users
+
+    public allUsers() {return [];}
+
+// função que coleta todos os users
+
+    public getUsers() {return [];}
+
+}
